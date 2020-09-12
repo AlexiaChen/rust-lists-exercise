dbbe515492c42ada663e4cc9fc107d5c26a67a93
MathxH Chen <brainfvck@foxmail.com>
2020-09-12 20:57:20 +0800

    Impl basic func for persistence list

--- src/lib/persistence_list.rs
+++ src/lib/persistence_list.rs
@@ -20,16 +20,85 @@ struct Node<T> {
 }
 
 impl<T> List<T> {
+    
+    /// single instance creator
+    /// ### Example
+    /// ```rust
+    /// use mylist::persistence_list::List;
+    /// let list = List::<i32>::new();
+    /// ```
     pub fn new() -> Self {
         List { head: None }
     }
 
+    /// append element to list head
+    /// ### Example
+    /// ```rust
+    /// use mylist::persistence_list::List;
+    /// let list = List::new();
+    /// let list = list.append(5).append(4);
+    /// assert_eq!(list.head(), Some(&4));
+    /// ```
     pub fn append(&self, element: T) -> List<T> {
         List {
             head: Some(Rc::new(Node {
                 element: element,
-                next: self.head.clone()
-            }))
+                next: self.head.clone(),
+            })),
         }
+    }
+
+    /// return rest of list except to head 
+    /// ### Example
+    /// ```rust
+    /// use mylist::persistence_list::List;
+    /// let list = List::new();
+    /// let list = list.append(5).append(4).append(3);
+    /// let list = list.tail();
+    /// assert_eq!(list.head(), Some(&4));
+    /// ```
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+    
+    /// return head value of list
+    /// ### Example
+    /// ```rust
+    /// use mylist::persistence_list::List;
+    /// let list = List::<i32>::new();
+    /// assert_eq!(list.head(), None);
+    /// ```
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.element)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.append(1).append(2).append(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
     }
 }
