0682a81bab3b0d603031104e118e89123aa8c045
MathxH Chen <brainfvck@foxmail.com>
2020-09-20 17:46:10 +0800

    Add example for deque API

--- src/lib/bad_deque.rs
+++ src/lib/bad_deque.rs
@@ -9,13 +9,19 @@ pub struct Deque<T> {
 type NodeRcCell<T> = Rc<RefCell<Node<T>>>;
 type Link<T> = Option<NodeRcCell<T>>;
 
-pub struct Node<T> {
+struct Node<T> {
     element: T,
     prev: Link<T>,
     next: Link<T>,
 }
 
 impl<T> Deque<T> {
+    /// create new empty deque
+    /// ### Example
+    /// ```rust
+    /// use mylist::bad_deque::Deque;
+    /// let deque = Deque::<i32>::new();
+    /// ```
     pub fn new() -> Self {
         Deque {
             head: None,
@@ -23,6 +29,13 @@ impl<T> Deque<T> {
         }
     }
 
+    /// push an element in front of deque
+    /// ### Example
+    /// ```rust
+    /// use mylist::bad_deque::Deque;
+    /// let mut deque = Deque::new();
+    /// deque.push_front(3);
+    /// ```
     pub fn push_front(&mut self, element: T) {
         let new_node = Node::new(element);
         match self.head.take() {
@@ -37,6 +50,29 @@ impl<T> Deque<T> {
             }
         }
     }
+
+    /// pop an element in front of deque
+    /// ### Example
+    /// ```rust
+    /// use mylist::bad_deque::Deque;
+    /// let mut deque = Deque::new();
+    /// deque.push_front(3);
+    /// assert_eq!(deque.pop_front(), Some(3));
+    /// ```
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|head_node| {
+            match head_node.borrow_mut().next.take() {
+                Some(next_node) => {
+                    next_node.borrow_mut().prev.take();
+                    self.head = Some(next_node);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(head_node).ok().unwrap().into_inner().element
+        })
+    }
 }
 
 impl<T> Node<T> {
@@ -46,5 +82,34 @@ impl<T> Node<T> {
             prev: None,
             next: None,
         }))
+    }
+}
+
+impl<T> Drop for Deque<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Deque;
+
+    #[test]
+    fn basics() {
+        let mut deque = Deque::new();
+        assert_eq!(deque.pop_front(), None);
+
+        deque.push_front(3);
+        deque.push_front(4);
+        deque.push_front(5);
+
+        assert_eq!(deque.pop_front(), Some(5));
+        assert_eq!(deque.pop_front(), Some(4));
+        deque.push_front(1);
+
+        assert_eq!(deque.pop_front(), Some(1));
+        assert_eq!(deque.pop_front(), Some(3));
+        assert_eq!(deque.pop_front(), None);
     }
 }
